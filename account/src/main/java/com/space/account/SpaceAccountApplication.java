dc62113811c145db80d9997a4ffa898afa5e8e9b
jiangshengyuan <1720668440@qq.com>
2025-06-07 02:36:48 +0800

    init

---
+++ account/src/main/java/com/space/account/SpaceAccountApplication.java
@@ -0,0 +1,11 @@
+package com.space.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceAccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceAccountApplication.class, args);
+    }
+}
--- store/src/main/java/com/space/store/SpaceStoreApplication.java
+++ store/src/main/java/com/space/store/SpaceStoreApplication.java
@@ -1,11 +1,11 @@
-package com.space.example;
+package com.space.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpaceApplication {
+public class SpaceStoreApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpaceApplication.class, args);
+        SpringApplication.run(SpaceStoreApplication.class, args);
     }
 }
