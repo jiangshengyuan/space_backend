2df61d4453136cbe8a1a01f1c1a5b7797a83fda4
jiangshengyuan <1720668440@qq.com>
2025-06-07 03:52:31 +0800

    init

--- sql/account.sql
+++ sql/account.sql
@@ -3,7 +3,8 @@ use t_space;
 -- 登录信息表
 create table if not exists t_space.login_info
 (
-    id             bigint primary key not null auto_increment,
+    id             bigint primary key not null auto_increment comment '默认自增id',
+    li_id  varchar(32)        not null comment '表ID',
     login_account  varchar(32)        not null comment '登陆账号',
     login_name     varchar(32)        not null comment '登录名',
     login_password char(16)           not null comment '登录密码',
@@ -16,12 +17,12 @@ create table if not exists t_space.login_info
 -- 用户信息表
 create table if not exists t_space.user_info
 (
-    id                bigint primary key not null,
-    login_account     bigint comment '登录账号',
+    id                bigint primary key not null comment '默认自增id',
+    ui_id             varchar(32) comment '表ID',
     login_name        bigint comment '登录名',
     true_name         varchar(64) comment '真实姓名',
     nick_name         varchar(32) comment '昵称',
-    birthday          datetime comment '出生年月',
+    birth             datetime comment '出生年月：yyyy-MM-dd',
     sex               char(1)            not null comment '性别',
     id_card           varchar(32) comment '身份证号码',
     country           varchar(64) comment '国籍',
@@ -31,20 +32,22 @@ create table if not exists t_space.user_info
     certification     char(1)            not null comment '是否实名认证',
     create_time       datetime,
     update_time       datetime,
-    status            char(1)            not null comment '是否有效：0：用户注销，1:使用中，X：违规封禁，Z：系统自动清除'
+    valid            char(1)            not null comment '是否有效：0：用户注销，1:使用中，X：违规封禁，Z：系统自动清除'
 );
 
 -- 角色表
 create table if not exists t_space.role_info
 (
-    id        bigint primary key not null,
+    id        bigint primary key not null comment '默认自增id',
+    ri_id             varchar(32) comment '表ID',
     role_name char(16)           not null comment '角色名:administrator',
     status    char(1)            not null comment '是否有效：0：用户注销，1:使用中，X：违规封禁，Z：系统自动清除'
 );
 -- 用户&角色表
 create table if not exists t_space.user_role
 (
-    id      bigint primary key not null,
+    id      bigint primary key not null comment '默认自增id',
+    ur_id             varchar(32) comment '表ID',
     user_id bigint             not null,
     role_id bigint             not null,
     status  char(1)            not null comment '是否有效：0：用户注销，1:使用中，X：违规封禁，Z：系统自动清除'
@@ -52,7 +55,8 @@ create table if not exists t_space.user_role
 -- 权限表
 create table if not exists t_space.role_authority
 (
-    id             bigint primary key not null,
+    id             bigint primary key not null comment '默认自增id',
+    ra_id             varchar(32) comment '表ID',
     role_id        bigint             not null,
     authority_code varchar(16)        not null comment '权限码',
     authority_name char(16)           not null comment '权限名',
@@ -61,7 +65,7 @@ create table if not exists t_space.role_authority
 -- 收货地址表
 create table if not exists t_space.delivery_address
 (
-    id             bigint primary key not null,
+    id             bigint primary key not null comment '默认自增id',
     user_id        bigint             not null comment '用户id',
     location       varchar(64) comment '城市',
     detail_address varchar(64) comment '详细地址',
@@ -76,7 +80,8 @@ create table if not exists t_space.delivery_address
 -- 优惠券表
 create table if not exists t_space.coupon
 (
-    id               bigint primary key not null,
+    id               bigint primary key not null comment '默认自增id',
+    c_id          bigint             not null comment '表id',
     user_id          bigint             not null comment '用户id',
     discount_type    char(1)            not null comment '打折方式',
     discount_amount  decimal(10, 2) comment '打折金额',
